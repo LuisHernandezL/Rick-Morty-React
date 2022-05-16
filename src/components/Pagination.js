c2581c38a303946fef614925a257d37258e60bf8
LuisHernandezL <luishernandz2503@gmail.com>
2022-05-16 11:48:36 -0400

    working netlify fix

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -14,7 +14,7 @@ const Pagination = ({postPerPage,totalPost ,paginate}) => {
             <ul className="pagination">
                 {pageNumbers.map(number=>(
                     <li key={number} className="page-item">
-                        <a onClick={()=>paginate(number)} href="#!" className='page-link'>
+                        <a onClick={()=>paginate(number)} href="!#" className='page-link'>
                             {number}
                         </a>
                     </li>
